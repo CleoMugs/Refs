24577dc78d47b45d88a062471bab04d011a41284
CleoMugs <cleophas.mugeni@gmail.com>
2021-04-02 19:45:09 +0300

    git cheat sheet

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+# ECHO COMMAND
+echo Hello World!
+
+#VARIABLES
+#Uppercase by convention
+
+#NAME="Cleo"
+echo "My name is ${NAME}"
+
+#USER INPUT
+read -p "Enter your name: " NAME
+echo "Hello $NAME, nice to meet you"
